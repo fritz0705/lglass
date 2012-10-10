15eccd91df9bf157bb13849395c45a13939f26ec
Fritz Grimpen <fritz@grimpen.net>
2012-10-10 22:21:05 +0000

    Removed useless __init__ code

--- lglass/__init__.py
+++ lglass/__init__.py
@@ -1,21 +1,5 @@
 # coding: utf-8
 
-import json
+import lglass.route
 import lglass.bird
 import lglass.rpsl
-
-config = None
-bird_client = None
-registry = None
-
-try:
-	with open("lglass.json") as f:
-		config = json.load(f)
-	bird_client = bird.Client(
-		birdc=config["programs"]["birdc"],
-		default_table=config["bird"]["table"],
-		route_filter=bird.BGPRoute.type_filter
-	)
-	registry = rpsl.Database(config["registry"])
-except:
-	pass
