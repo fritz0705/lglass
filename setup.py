af3e467e4ccfe9031dab862007d970a924271b01
Fritz Grimpen <fritz@grimpen.net>
2013-05-12 11:33:28 +0000

    Add simple setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import disutils.core
+
+with open("README.md") as f:
+	long_description = f.read()
+
+distutils.core.setup(
+	name="lglass",
+	version="1.0dev1",
+	packages=["lglass"],
+	author="Fritz Grimpen",
+	author_email="fritz@grimpen.net",
+	url="https://github.com/fritz0705/lglass.git",
+	license="http://opensource.org/licenses/MIT",
+	description="lglass is a library which provides simple tools for working with RPSL databases",
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Operating System :: POSIX",
+		"Programming Language :: Python :: 3.3",
+		"Topic :: Internet :: Log Analysis"
+	],
+	long_description=long_description,
+	install_requires=[
+		"netaddr>=0.7.10"
+	]
+)
+
