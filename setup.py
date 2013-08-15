d8df6f0608538d11db4ab92285ed8913e4709259
Fritz Grimpen <fritz@grimpen.net>
2013-08-15 09:17:36 +0000

    Use setuptools for setup()

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
-import distutils.core
+import setuptools
 
 with open("README.md") as f:
 	long_description = f.read()
 
-distutils.core.setup(
+setuptools.setup(
 	name="lglass",
 	version="1.0dev",
-	packages=["lglass"],
+	packages=[
+		"lglass",
+		"lglass.generators",
+		"lglass.tools",
+		"lglass.web",
+	],
 	author="Fritz Grimpen",
 	author_email="fritz@grimpen.net",
 	url="https://github.com/fritz0705/lglass.git",
@@ -29,6 +34,16 @@ distutils.core.setup(
 		"bottle>=0.11.6",
 		"Jinja2>=2.7",
 		"MarkupSafe>=0.18"
-	]
+	],
+	entry_points={
+		"console_scripts": [
+			"lglass-regtool = lglass.tools.regtool:main",
+			"lglass-roagen = lglass.tools.roagen:main",
+			"lglass-rpsl = lglass.rpsl:main",
+			"lglass-web = lglass.web.__main__:main",
+			"lglass-whoisd = lglass.whoisd:main",
+			"lglass-zonegen = lglass.tools.zonegen:main"
+		]
+	}
 )
 
