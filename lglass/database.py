96adc6e4ccd7f3b07817f8c6b2803823a6e06468
Fritz Grimpen <fritz@grimpen.net>
2013-08-16 17:55:24 +0000

    Implement database for Redis

--- lglass/database.py
+++ lglass/database.py
@@ -3,6 +3,7 @@
 import os.path
 import time
 import socket
+import json
 
 import netaddr
 
@@ -658,4 +659,105 @@ class WhoisClientDatabase(Database):
 
 	def delete(self):
 		raise NotImplementedError("delete() is not supported for WhoisClientDatabase")
+
+try:
+	import redis
+except ImportError:
+	redis = None
+
+class RedisDatabase(Database):
+	def __init__(self, db, _redis, timeout=600, prefix="lglass:"):
+		if isinstance(_redis, redis.Redis):
+			self.redis = _redis
+		elif isinstance(_redis, dict):
+			self.redis = redis.Redis(**_redis)
+		elif isinstance(_redis, tuple):
+			self.redis = redis.Redis(*_redis)
+		else:
+			raise TypeError("Expected redis.Redis, dict or tuple as redis instance, got {}".format(type(_redis)))
+
+		self.database = db
+		self.timeout = timeout
+		self.prefix = prefix
+
+	def get(self, type, primary_key):
+		obj = self.redis.get(self._key_for(type, primary_key))
+		if obj is None:
+			obj = self.database.get(type, primary_key)
+			self.redis.set(self._key_for(type, primary_key),
+				self._serialize(obj),
+				ex=self.timeout)
+		else:
+			obj = self._deserialize(obj.decode())
+		return obj
+	
+	def list(self):
+		listing = self.redis.get(self._key_for_list())
+		if listing is None:
+			listing = self.database.list()
+			self.redis.set(self._key_for_list(),
+				self._serialize_listing(listing),
+				ex=self.timeout)
+		else:
+			listing = self._deserialize_listing(listing.decode())
+		return listing
+
+	def find(self, key, types=None):
+		if types is None:
+			types = self.object_types
+		results = self.redis.get(self._key_for_find(key, types))
+		if results is None:
+			results = self.database.find(key, types)
+			self.redis.set(self._key_for_find(key, types),
+					self._serialize_find(results),
+					ex=self.timeout)
+		else:
+			results = self._deserialize_find(results.decode())
+		return results
+
+	def save(self, obj):
+		self.database.save(obj)
+		self.redis.set(self._key_for(type, primary_key),
+			self._serialize(obj),
+			ex=self.timeout)
+
+	def delete(self, type, primary_key):
+		self.database.delete(type, primary_key)
+		self.redis.delete(self._key_for(type, primary_key))
+
+	def _serialize(self, obj):
+		return json.dumps(obj.to_json_form())
+
+	def _deserialize(self, string):
+		return lglass.rpsl.Object(json.loads(string))
+
+	def _serialize_listing(self, listing):
+		result = [[key, value] for key, value in listing]
+		return json.dumps(result)
+	
+	def _deserialize_listing(self, string):
+		listing = json.loads(string)
+		return [(key, value) for key, value in listing]
+
+	def _serialize_find(self, finds):
+		result = [list(obj.real_spec) for obj in finds]
+		return json.dumps(result)
+
+	def _deserialize_find(self, string):
+		finds = json.loads(string)
+		result = []
+		for spec in finds:
+			result.append(self.get(*spec))
+		return result
+
+	def _key_for_find(self, key, types):
+		return "{prefix}find+{key}+{types}".format(prefix=self.prefix,
+			key=key, types=",".join(types))
+	
+	def _key_for_list(self):
+		return "{prefix}list".format(prefix=self.prefix)
+
+	def _key_for(self, type, primary_key):
+		return "{prefix}{type}+{primary_key}".format(prefix=self.prefix,
+			type=type, primary_key=primary_key)
 
