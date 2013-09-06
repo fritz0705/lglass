f756f977bc9728e677921283807b80bc32ce4397
Fritz Grimpen <fritz@grimpen.net>
2013-09-06 18:26:38 +0000

    Simplify lmdb usage

--- lglass/database/lmdb.py
+++ lglass/database/lmdb.py
@@ -34,21 +34,18 @@ class LMDBDatabase(lglass.database.base.Database):
 	
 	def get(self, type, primary_key):
 		with self.env.transaction(lmdb.MDB_RDONLY) as txn:
-			with txn.primary_database as db:
-				obj = db["+".join((type, primary_key))].decode()
+			obj = txn["+".join((type, primary_key))].decode()
 		return lglass.rpsl.Object.from_string(obj)
 
 	def list(self):
 		with self.env.transaction(lmdb.MDB_RDONLY) as txn:
-			with txn.primary_database as db:
-				directory = db["+directory"].decode()
+			directory = txn["+directory"].decode()
 		for entry in json.loads(directory):
 			yield tuple(entry)
 
 	def find(self, primary_key, types=None):
 		with self.env.transaction(lmdb.MDB_RDONLY) as txn:
-			with txn.primary_database as db:
-				directory = db["+directory"].decode()
+			directory = txn["+directory"].decode()
 		for entry in json.loads(directory):
 			if types is not None and entry[0] not in types:
 				continue
@@ -58,20 +55,18 @@ class LMDBDatabase(lglass.database.base.Database):
 
 	def save(self, object):
 		with self.env.transaction() as txn:
-			with txn.primary_database as db:
-				db["+".join(object.spec)] = object.pretty_print()
-				directory = json.loads(db["+directory"].decode())
-				if list(object.spec) not in directory:
-					directory.append(list(object.spec))
-					db["+directory"] = json.dumps(directory)
+			txn["+".join(object.spec)] = object.pretty_print()
+			directory = json.loads(txn["+directory"].decode())
+			if list(object.spec) not in directory:
+				directory.append(list(object.spec))
+				txn["+directory"] = json.dumps(directory)
 
 	def delete(self, type, primary_key):
 		with self.env.transaction() as txn:
-			with txn.primary_database as db:
-				del db["+".join((type, primary_key))]
-				directory = json.loads(db["+directory"].decode())
-				directory.remove([type, primary_key])
-				db["+directory"] = json.dumps(directory)
+			del txn["+".join((type, primary_key))]
+			directory = json.loads(txn["+directory"].decode())
+			directory.remove([type, primary_key])
+			txn["+directory"] = json.dumps(directory)
 
 	def __hash__(self):
 		return hash(self.env)
