672e25b347b4c590996dfa1391992995d84a723f
Fritz Grimpen <fritz@grimpen.net>
2014-09-14 19:57:46 +0000

    Fix reverse zone generator

--- lglass/rpsl.py
+++ lglass/rpsl.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 
 from lglass.object import Object as RawObject, SchemaValidationError, SchemaObject, \
-		RIPESchemaObject, SchemaKeyConstraint, SchemaValidator, parse_object, main
+		RIPESchemaObject, SchemaKeyConstraint, SchemaValidator, parse_object, main, \
+		inetnum_cidrs, inetnum_range
 
 class Object(RawObject):
 	pass
