dca807661838e8fe5c73545495936a9f4bbb81a1
Fritz Grimpen <fritz@grimpen.net>
2017-08-01 22:17:22 +0000

    Revert "TYPO Use correct variables"
    This reverts commit 9d8c56e73a65c3c37f206985eceeba05cc28da89.

--- lglass/whois/engine.py
+++ lglass/whois/engine.py
@@ -289,7 +289,7 @@ class WhoisEngine(object):
                     found += 1
             if found:
                 found_levels += 1
-            if found == found_levels:
+            if found_levels == levels:
                 break
 
     def query_less_specifics(self, obj, levels=1, database=None):
