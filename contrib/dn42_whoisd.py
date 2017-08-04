7eb23d0874a76683ecb57474c22d2f732626ace2
Fritz Grimpen <fritz@grimpen.net>
2017-08-04 22:41:10 +0000

    Add type hints for DN42 and RDNS domains

--- contrib/dn42_whoisd.py
+++ contrib/dn42_whoisd.py
@@ -5,11 +5,14 @@ import argparse
 import asyncio
 
 import lglass.dn42
+import lglass.proxy
 import lglass.whois.engine
 import lglass.whois.server
 
 def create_database(db_path):
-    return lglass.dn42.DN42Database(db_path)
+    db = lglass.dn42.DN42Database(db_path)
+    return lglass.proxy.CacheProxyDatabase(db,
+            lifetime=600)
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="DN42 Whois server")
@@ -22,6 +25,9 @@ if __name__ == "__main__":
     db = create_database(args.database)
     engine = lglass.whois.engine.WhoisEngine(db)
     engine.type_hints[r"[0-9A-Za-z]+-DN42$"] = {"role", "person"}
+    engine.type_hints[r".*\.in-addr\.arpa$"] = {"domain"}
+    engine.type_hints[r".*\.ip6\.arpa$"] = {"domain"}
+    engine.type_hints[r".*\.dn42$"] = {"domain"}
     server = lglass.whois.server.SimpleWhoisServer(engine)
 
     loop = asyncio.get_event_loop()
